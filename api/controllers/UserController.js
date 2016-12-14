f932beba39ca3ddd106fd680f20256734dccd369
Baptiste <baptiste.lanusse@ynov.com>
2016-12-14 14:30:51 +0100

    correction /user/me

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -7,12 +7,13 @@
 
 module.exports = {
 	me: function (req, res) {
-		Log.create({
+		/*Log.create({
 			description: 'A user has been retrieved by token: ' + user.email
 		}).exec(function(err, log){
 			if (err) { return res.serverError(err); }
 			return res.send(req.user);
-		});
+		});*/
+		return res.send(req.user);
 	},
 	getUser: function (req, res) {
 		User.findOne({ id: req.param('userId') }).exec(function(err, user){
