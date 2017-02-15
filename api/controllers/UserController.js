ee1fd7feb80998c5de190d130f13b0a0e38e8bc6
Baptiste <baptiste.lanusse@ynov.com>
2017-02-15 11:41:34 +0100

    nettoyage

--- api/controllers/UserController.js
+++ api/controllers/UserController.js
@@ -6,26 +6,4 @@
  */
 
 module.exports = {
-	getUser: function (req, res) {
-		User.findOne({ id: req.param('userId') }).exec(function(err, user){
-			if (err) { return res.serverError(err); }
-			if (!user) { return res.notFound('No user found for this id'); }
-
-			Log.create({
-				description: 'A user has been retrieved by id: ' + user.email
-			}).exec(function(err, log){
-				if (err) { return res.serverError(err); }
-
-				return res.ok(user);
-			});
-		});
-	},
-	getUsers: function (req, res) {
-		User.find().exec(function(err, users) {
-			if (err) { return res.serverError(err); }
-			if (!users) { return res.serverError('No users'); }
-
-			return res.ok(users);
-		});
-	}
 };
